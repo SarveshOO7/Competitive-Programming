e3ad8f5af17173cc20a974a25afce8d0ea7419f7
Sarvesh Tandon <37467103+SarveshOO7@users.noreply.github.com>
2020-11-29 13:50:52 +0530

    IDE setup for CP

--- cp3 problems/intro/11799HorrorDash.cpp
+++ cp3 problems/intro/11799HorrorDash.cpp
@@ -3,18 +3,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-int main(){
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
     ll t, temp;
-    cin>>t;
-    for(ll i=1;i<=t;i++){
+    cin >> t;
+    for (ll i = 1; i <= t; i++)
+    {
         ll n;
-        vector< ll > v;
-        cin>>n;
-        for(int j=0;j<n;j++){
-            cin>>temp;
+        vector<ll> v;
+        cin >> n;
+        for (int j = 0; j < n; j++)
+        {
+            cin >> temp;
             v.push_back(temp);
         }
-        cout<<"Case "<<i<<": "<< *max_element(v.begin(), v.end())<<endl;
+        cout << "Case " << i << ": " << *max_element(v.begin(), v.end()) << endl;
     }
 
     return 0;
